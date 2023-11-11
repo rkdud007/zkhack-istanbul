6f7d98b4992248127067afac0ed10200188dc85e
Pia <gayeongparkk@gmail.com>
2023-11-11 10:45:28 +0300

    fix

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         DEVELOPMENT_API_URL: "http://localhost:5001",
-        PRODUCTION_API_URL: "https://zkhack-istanbul-r5zg.vercel.app/",
+        PRODUCTION_API_URL: "https://zkhack-istanbul-r5zg.vercel.app",
         ALGOLIA_APP_ID: process.env.ALGOLIA_APP_ID,
         ALGOLIA_PUBLIC_API_KEY: process.env.ALGOLIA_PUBLIC_API_KEY,
     },
